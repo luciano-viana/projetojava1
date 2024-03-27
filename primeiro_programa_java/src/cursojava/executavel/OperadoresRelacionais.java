afd3cca0383e186daf0192105a8fe3ec1b61cfc2
luciano viana <luciano viana@Viana>
2024-03-27 08:39:51 -0300

    Utilizando Operadores Relacionais

---
+++ primeiro_programa_java/src/cursojava/executavel/OperadoresRelacionais.java
@@ -0,0 +1,33 @@
+package cursojava.executavel;
+
+public class OperadoresRelacionais {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Operadores Relacionais 
+		 * == Igual 
+		 * != Diferente 
+		 * < Menor 
+		 * > Maior 
+		 * <= Menor Igual
+		 * >= Maior Igual
+		 */
+
+		int nota1 = 92;
+		int nota2 = 92;
+		
+		if (nota1 == nota2) {
+			System.out.println("Nota1: " + nota1);
+			System.out.println("Nota2: " + nota2);
+			System.out.println("Nota1 é igual a Nota2");
+		} else {
+			System.out.println("Nota1: " + nota1);
+			System.out.println("Nota2: " + nota2);
+            System.out.println("Nota1 não é Igual a Nota2");
+		}
+
+		
+	}
+
+}
