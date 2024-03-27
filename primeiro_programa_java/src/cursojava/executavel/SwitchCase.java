2c963fd20e221b8650aa62b6cec7f012153fb070
luciano viana <luciano viana@Viana>
2024-03-27 08:18:39 -0300

    Utilizado SWITCH CASE

---
+++ primeiro_programa_java/src/cursojava/executavel/SwitchCase.java
@@ -0,0 +1,39 @@
+package cursojava.executavel;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+
+		int dia = 1;
+
+		/*SWITCH CASE: Utilizado para Operações Exatas*/
+		switch (dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terça");
+			break;
+		case 4:
+			System.out.println("Quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sábado");
+			break;
+		default:
+			System.out.println("Dia não encontrado");
+			break;
+		}
+
+	}
+
+}
