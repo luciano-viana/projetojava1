3f34df0eb43e4c71a74ec5c49634e05ef46a2e30
luciano viana <luciano viana@Viana>
2024-03-30 11:07:02 -0300

    Estrutura de repetição WHILE

---
+++ primeiro_programa_java/src/cursojava/executavel/EstruturaRepeticaoWHILE.java
@@ -0,0 +1,19 @@
+package cursojava.executavel;
+
+public class EstruturaRepeticaoWHILE {
+
+	public static void main(String[] args) {
+
+		/* Estrutura de repeticação While */
+
+		int numero = 1;
+
+		while (numero <= 10) { /*Verifica e depois executa*/
+
+			System.out.println("Numero atual é: " + numero);
+			numero ++;
+
+		}
+	}
+
+}
