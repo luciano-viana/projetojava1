4b6490016b62b53daf438f4d16f7c009213784b4
luciano viana <luciano viana@Viana>
2024-03-30 13:06:31 -0300

    Módulo: Resto da divisão %

---
+++ primeiro_programa_java/src/cursojava/executavel/UtilizandoMOD.java
@@ -0,0 +1,18 @@
+package cursojava.executavel;
+
+public class UtilizandoMOD {
+
+	public static void main(String[] args) {
+
+		/* Módulo: Resto da divisão % */
+
+		double numero1 = 10;
+		double numero2 = 3;
+		double resto = 0;
+
+		resto = numero1 % numero2;
+
+		System.out.println("Resto da divisão de " + numero1 + " por " + numero2 + " é: " + resto);
+
+	}
+}
