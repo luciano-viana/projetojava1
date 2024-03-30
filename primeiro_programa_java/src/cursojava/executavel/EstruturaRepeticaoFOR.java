5cb76badbb841b7fff3c53eb9bd2cbca83ee6a31
luciano viana <luciano viana@Viana>
2024-03-30 11:40:28 -0300

    Estrutura de repetição FOR

---
+++ primeiro_programa_java/src/cursojava/executavel/EstruturaRepeticaoFOR.java
@@ -0,0 +1,15 @@
+package cursojava.executavel;
+
+public class EstruturaRepeticaoFOR {
+	
+	public static void main(String[] args) {
+		
+		/*Estrutura de repetição FOR, utilizado para percorrer um número, lista de dados e outros... */
+		
+		for (int i = 10; i >= 1 ; i--) {
+			System.out.println("Número atual é : " + i);
+			
+		}
+	}
+
+}
