0b54bfdc21130a03cc6d908afaa769ca73a6778f
luciano viana <luciano viana@Viana>
2024-04-03 08:26:41 -0300

    classe Aluno

---
+++ primeiro_programa_java/src/cursojava/classes/Aluno.java
@@ -0,0 +1,15 @@
+package cursojava.classes;
+
+/*Está é nossa classe/objeto que representa o Aluno*/
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	
+	
+	
+
+}
