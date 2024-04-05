9e9b310c45bc0f0b909d8fa7c91545b289cb7d32
luciano viana <luciano viana@Viana>
2024-04-05 08:40:53 -0300

    Instância, Referência, Atributo

---
+++ primeiro_programa_java/src/cursojava/executavel/PrimeiraClasseJava2.java
@@ -0,0 +1,31 @@
+package cursojava.executavel;
+
+import cursojava.classes.Aluno;
+
+public class PrimeiraClasseJava2 {
+	
+	public static void main(String[] args) {
+		
+		/*Objeto ainda não existe na memória*/
+		Aluno aluno0;
+		
+		
+		/*Agora sim temos um objeto real na memória*/
+		/*Construtor padrão*/
+		/*new Aluno() é uma instânciar (Criação de Objeto)*/
+		/*Exemplo: aluno1 é uma variável referência para o objetivo aluno*/
+		/*Instância 'construtor': São os contrutores para construir o objeto, exemplo "new Aluno()".
+		 *Referência: São as variáveis que faz referência para os objetos, exemplo: "aluno1".
+		 *Atributo: São os atributos das caracteristicas 'nome, idade...' que representa a pessoa no mundo real, no caso da classe Aluno*/
+		Aluno aluno1 = new Aluno(); /*Aqui será o João*/
+		Aluno aluno2 = new Aluno(); /*Aqui será o Pedro*/
+		Aluno aluno3 = new Aluno(); /*Aqui será o Luciano*/
+		
+		/*Construtor com uma parâmetro*/
+		Aluno aluno4 = new Aluno("Maria");
+		
+		/*Construtor com 2 parâmetros*/
+		Aluno aluno5 = new Aluno("Lais", 23);
+	}
+
+}
