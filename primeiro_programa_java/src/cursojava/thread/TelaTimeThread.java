d97de41b41ae50c368ce82a1803fa721d687c2e1
luciano-viana <lucianoviana12@gmail.com>
2024-06-14 17:53:25 -0300

    adicionado classe TelaTimeThread

---
+++ primeiro_programa_java/src/cursojava/thread/TelaTimeThread.java
@@ -0,0 +1,27 @@
+package cursojava.thread;
+
+import java.awt.Dimension;
+
+//Swing é um framework para clirar as aplicações em Desktop
+import javax.swing.JDialog;
+
+public class TelaTimeThread extends JDialog {
+	
+	public TelaTimeThread() {/*Constructor executa o que tiver dentro no momento da abertura ou execução*/
+		//início, configurações inicias da tela
+		setTitle("Minha tela de time com Thread");
+		setSize(new Dimension(400, 400));//Tamanho tela
+		setLocationRelativeTo(null);//Centralizar tela
+		setResizable(false);//Travar alteração na tela após executada
+		/*Primeira parte concluida*/
+		
+		
+		
+		
+		
+		//Sempre será o últmo comando
+		setVisible(true);/*Torna a tela visivel para o usuário*/
+	}
+	
+
+}
