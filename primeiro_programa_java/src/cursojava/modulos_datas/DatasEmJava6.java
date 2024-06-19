2f33230a85bf911b1448f1c4b84010764ced70e7
luciano-viana <lucianoviana12@gmail.com>
2024-06-19 05:37:24 -0300

    Formatação de data e hora com LocalDate, LocalTime e LocalDateTime

---
+++ primeiro_programa_java/src/cursojava/modulos_datas/DatasEmJava6.java
@@ -0,0 +1,24 @@
+package cursojava.modulos_datas;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class DatasEmJava6 {
+	
+	public static void main(String[] args) {
+		
+		/*Nova API de data a partir do Java 8*/
+		
+		LocalDate dataAtual = LocalDate.now();
+		System.out.println("Data Atual: " + dataAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		
+		LocalTime horaAtual = LocalTime.now();
+		System.out.println("Hora Atual: " + horaAtual.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
+		
+		LocalDateTime dataAtualHoraAtual = LocalDateTime.now();
+		System.out.println("Data e Hora Atual: " + dataAtualHoraAtual.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
+	}
+
+}
