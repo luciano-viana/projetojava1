d8f17105ba6c4f29a7eb43566aca6c10f8d9be02
luciano-viana <lucianoviana12@gmail.com>
2024-06-24 08:50:36 -0300

    Módulo 17 - Estrutura, Conceitos, Fundamentos e Funcionamento de Sistemas

---
+++ primeiro_programa_java/src/curso/java/ModulosDeEstudos/Modulo17.java
@@ -0,0 +1,33 @@
+package curso.java.ModulosDeEstudos;
+
+public class Modulo17 {
+	/*#Módulo 17 - Estrutura, Conceitos, Fundamentos e Funcionamento de Sistemas#
+	17.1 - Introdução - O que veremos neste módulo - OK		
+	17.2 - Como funciona um sistema Desktop? - OK			
+	17.3 - Como funciona um sistema WEB? - OK			
+	17.4 - Camadas de um sistema Desktop - OK			
+	17.5 - Camadas sistema Web - OK			
+	17.6 - NÃO, NÃO E NÃÃÃOOOO Pule Etapas na Programação - OK			
+	17.7 - Tenha calma em todas as horas para programar com sucesso - OK			
+	17.8 - Estudar, Pesquisar e aprender sem parar... - OK			
+	17.9 - Mentalidade de um Programador de verdade	- OK		
+	17.10 - Foque na lógica de programação - OK			
+	17.11 - Foque em Algoritmos - OK			
+	17.12 - Seja especialista em uma ÚNICA linguagem - OK			
+	17.13 - Você precisa ter FOCO e objetivo - OK			
+	17.14 - Domine POO - Programação Orientada a Objetos - OK			
+	17.15 - Por onde começar no Java Web? - OK			
+	17.16 - Modelo mental de estudos em JSP - JavaServer Pages - OK			
+	17.17 - Modelo mental de estudos em Spring MVC - OK			
+	17.18 - Modelo mental de estudos em JavaServer Faces - OK			
+	17.19 - Modelo mental de estudos em REST e RESTful - OK			
+	17.20 - Modelo mental de estudos Java Web			
+	17.21 - Conhecendo básico de HTML			
+	17.22 - JavaScript não é Java - Vamos aprender a diferença			
+	17.23 - Vamos aprender o que é CSS			
+	17.24 - Entendendo arquitetura em (MVC ) Model - View - Controller			
+	17.25 - Frameworks ORM - Hibernate e JPA			
+	17.26 - Funcionamento dos Web Services			
+	17.27 - Importância do banco de dados em um sistema			
+	*/
+}
