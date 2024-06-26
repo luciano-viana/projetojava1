832a38947ce2f2250f4abc04bafe48c6cc3d1e8e
luciano-viana <lucianoviana12@gmail.com>
2024-06-26 08:52:19 -0300

    Configurando o ambiente Java Web e Spring Framework

---
+++ primeiro_programa_java/src/curso/java/ModulosDeEstudos/Modulo18.java
@@ -0,0 +1,8 @@
+package curso.java.ModulosDeEstudos;
+
+public class Modulo18 {
+	/*#Configurando o ambiente Java Web e Spring Framework 
+	 * Instalado Apache Tomcast e postgresql-15.7-2 
+	 * Configurado ambiente Windows para utilização do Apache Tomcast
+	 */
+}
